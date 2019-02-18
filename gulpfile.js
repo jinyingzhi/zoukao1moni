521b06527e9e8257c5bb7db0a62796c968394471
jinyingzhi <1076497058@qq.com>
2019-02-18 09:26:07 +0800

    分支

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,38 @@ var concat = require('gulp-concat');
 var uglifyJs = require('gulp-uglify');
 var cleanCss = require('gulp-clean-css');
 var webserver = require('gulp-webserver');
+
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+
+gulp.task('webserver', function() {
+    return gulp.src('./')
+        .pipe(webserver({
+            open: true,
+            port: 8080,
+            livereload: true,
+            middleware: function(req, res) {
+
+            }
+        }))
+})
+
+gulp.task('sass', function() {
+    return gulp.src('./src/scss/demo.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./src/css'))
+})
+
+gulp.task('css', function() {
+    return gulp.src('./src/css/*.css')
+        .pipe(cleanCss())
+        .pipe(gulp.dest('./src/css'))
+})
+
+gulp.task('js', function() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglifyJs())
+        .pipe(concat('new.js'))
+        .pipe(gulp.dest('./src/scripts/libs'))
+})
---
+++ src/scripts/libs/new.js
@@ -0,0 +1,3 @@
+function a(){console.log("a.js")}a();
+function b(){console.log("b.js")}b();
+function index(){console.log("index.js")}index();
